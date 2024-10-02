274dcea8fbc120c63cf9bd12d325297650dc68e7
GHERY-HT <ghery.huanca.torrez1@gmail.com>
2024-10-02 02:42:07 -0400

    ImprovingConductorTable

--- SolutionTurybus/TuryBuzOLTP/Schema/Tables/Conductor.sql
+++ SolutionTurybus/TuryBuzOLTP/Schema/Tables/Conductor.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Conductor] (
-		NOT NULL CONSTRAINT PK_conductor PRIMARY KEY,
-		NOT NULL,
-		NOT NULL,
-		NOT NULL,
-		NOT NULL,
-		[rowversion]    [timestamp]      NOT NULL
+         [id]               [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_conductor PRIMARY KEY,  -- Cédula de identidad del conductor
+         [Apellidos]        [varchar](50)       NOT NULL,               -- Apellidos del conductor
+         [Nombre]           [varchar](50)       NOT NULL,               -- Nombre del conductor
+         [Telefono]         [varchar](25)       NOT NULL,               -- Teléfono del conductor                                
+         [Direccion]        [varchar](100)      NOT NULL,               -- Dirección del conductor
+         [rowversion]       [timestamp]         NOT NULL                -- Control de versiones (timestamp)
 );
 GO
+
--- SolutionTurybus/TuryBuzOLTP/Schema/Tables/Reparaciones.sql
+++ SolutionTurybus/TuryBuzOLTP/Schema/Tables/Reparaciones.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Reparaciones] (
     [id]               [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_reparaciones PRIMARY KEY,
     [id_revision]      [int]               NOT NULL,
-										   NOT NULL,
+--										   NOT NULL,
     [tiempo_empleado]  [time]              NOT NULL,
     [costo_reparacion] [decimal](10,2)     NOT NULL,
     [costo_repuestos]  [decimal](10,2)     NOT NULL,
