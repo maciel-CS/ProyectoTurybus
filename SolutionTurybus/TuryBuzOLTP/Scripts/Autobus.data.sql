37886f329e1257ed9ef40612360069c772a71f39
GHERY-HT <ghery.huanca.torrez1@gmail.com>
2024-10-02 00:54:39 -0400

    datos para tablas, Ghery

---
+++ SolutionTurybus/TuryBuzOLTP/Scripts/Autobus.data.sql
@@ -0,0 +1,19 @@
+﻿PRINT 'Populating Autobus table';
+SET NOCOUNT ON;
+
+-- Insertando datos en la tabla Autobus
+INSERT INTO Autobus (Matricula, Numero_de_plazas, Modelo, Fabricante, Descripcion_caracteristicas)
+VALUES 
+('6191-FNI', 40, 'FORTE', 'ISUZU', 'BUS PREMIUM'),
+('4123-ABC', 40, 'FASTER', 'ISUZU', 'Autobús moderno con asientos reclinables'),
+('5789-XYZ', 50, 'TRUST', 'HITACHI', 'Autobús de dos pisos con aire acondicionado y asientos reclinables'),
+('2023-POI', 36, 'ALLPATH', 'DYNA', 'Bus normal, asientos fijos'),
+('3666-WER', 40, 'RODEO', 'VOLVO', 'Autobús moderno con asientos reclinables'),
+('1124-FGC', 50, 'KING+', 'HYUNDAI', 'Autobús moderno con asientos reclinables'),
+('2568-ERN', 40, 'QUEEN++', 'HYUNDAI', 'Autobús moderno con asientos reclinables'),
+('4972-DFQ', 36, 'SOLDIER', 'KOMATSU', 'Bus normal, asientos fijos'),
+('2123-VDD', 40, 'CLIMBING-UP', 'SCANIA', 'Autobús moderno con asientos reclinables'),
+('3123-OPL', 50, 'ALMOST PLANE', 'CAT', 'Autobús de dos pisos con aire acondicionado y asientos reclinables');
+GO
+
+PRINT 'Data inserted into Revision table';
---
+++ SolutionTurybus/TuryBuzOLTP/Scripts/Conductor.data.sql
@@ -0,0 +1,18 @@
+﻿PRINT 'Populating Conductor table';
+SET NOCOUNT ON;
+
+INSERT INTO dbo.Conductor (Ci, Apellidos, Nombre, Telefono, Direccion)
+VALUES 
+('12345678', 'Rodriguez', 'Gabriela', '63417543', 'Calle R, Santa Cruz'),
+('87654321', 'Conde', 'Maciel', '72299441', 'Calle C, Cochabamba'),
+('11111111', 'Huanca', 'Ghery', '73280364', 'Avenida G, La Paz'),
+('87665794', 'Garcia', 'Mario', '67654320', 'Calle X, Cochabamba'),
+('78985496', 'Lopez', 'Tomas', '65444321', 'Calle L, Cochabamba'),
+('78985496', 'Gomez', 'Martha', '78434527', 'Calle L, Santa Cruz'),
+('87555799', 'Tohara', 'Sebastian', '60654145', 'Pasaje A, La Paz'),
+('32535797', 'Nunez', 'Fernando', '69841357', 'Calle R, Cochabamba'),
+('98432199', 'Cruz', 'Daniel', '70365112', 'Avenida Q, Sucre'),
+('15648973', 'Calderon', 'Roberto', '60005488', 'Calle K, La Paz');
+GO
+
+PRINT 'Data inserted into Revision table';
