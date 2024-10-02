27f097fd32362d67c5575b7818817d829b768a07
GHERY-HT <ghery.huanca.torrez1@gmail.com>
2024-10-01 23:08:14 -0400

    DataGaby

---
+++ SolutionTurybus/TuryBuzOLTP/Scripts/LugaresTuristicos.data.sql
@@ -0,0 +1,16 @@
+﻿PRINT 'Populating LugaresTuristicos table';
+SET NOCOUNT ON;
+
+SET IDENTITY_INSERT dbo.LugaresTuristicos ON;  
+
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Guembe')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Machia')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Lago Titicaca')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Ecoparque Las Orquideas')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Uyuni')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Mirador Illimani')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Isla del Sol')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Samaipata')
+INSERT INTO dbo.LugaresTuristicos(Descripcion) VALUES('Vallagrande')
+
+SET IDENTITY_INSERT dbo.LugaresTuristicos OFF;
