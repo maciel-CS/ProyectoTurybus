7f182c4425d60444dca4f02805431e95c92092f2
gabriela-re <73082628+gabriela-re@users.noreply.github.com>
2024-10-05 18:44:53 -0400

    Update Turista.data.sql

--- SolutionTurybus/TuryBuzOLTP/Scripts/Turista.data.sql
+++ SolutionTurybus/TuryBuzOLTP/Scripts/Turista.data.sql
@@ -4,10 +4,10 @@ SET NOCOUNT ON;
 -- Insertar datos en la tabla Turista
 INSERT INTO [dbo].[Turista] (ci, apellidos, nombre, telefono, direccion, nit)
 VALUES 
-('12345678', 'PÈrez', 'Juan', '555-1234', 'Calle Falsa 123', '1234567890'),
-('23456789', 'Gonz·lez', 'MarÌa', '555-5678', 'Avenida Siempre Viva 456', '2345678901'),
-('34567890', 'RodrÌguez', 'Carlos', '555-9876', 'Calle de la Libertad 789', '3456789012'),
-('45678901', 'Fern·ndez', 'Ana', '555-4321', 'Calle del Sol 321', '4567890123'),
-('56789012', 'LÛpez', 'Marta', '555-8765', 'Avenida del Parque 654', '5678901234');
+('12345678', 'P√©rez', 'Juan', '69080820', 'Calle Falsa 123', '1234567890'),
+('23456789', 'Gonz√°lez', 'Mar√≠a', '75874859', 'Avenida Siempre Viva 456', '2345678901'),
+('34567890', 'Rodr√≠guez', 'Carlos', '63258749', 'Calle de la Libertad 789', '3456789012'),
+('45678901', 'Fern√°ndez', 'Ana', '68524197', 'Calle del Sol 321', '4567890123'),
+('56789012', 'L√≥pez', 'Marta', '78549652', 'Avenida del Parque 654', '5678901234');
 
 SET IDENTITY_INSERT dbo.Turista OFF;
