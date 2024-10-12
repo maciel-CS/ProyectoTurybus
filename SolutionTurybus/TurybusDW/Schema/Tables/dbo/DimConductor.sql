3871c5f5a92036392c22b1a9067354cf5e83e8aa
GHERY-HT <ghery.huanca.torrez1@gmail.com>
2024-10-12 06:45:15 -0400

    etl conductor

--- SolutionTurybus/TurybusDW/Schema/Tables/dbo/DimConductor.sql
+++ SolutionTurybus/TurybusDW/Schema/Tables/dbo/DimConductor.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [dbo].[DimConductor] (
     ConductorSK INT IDENTITY(1,1) NOT NULL PRIMARY KEY,             -- Surrogate Key
-    ConductorID VARCHAR(20) NOT NULL,                                  -- ID de negocio
+    ConductorID INT NOT NULL,                                  -- ID de negocio
     CI VARCHAR(20) NOT NULL,                                           -- Número de cédula de identidad (no es clave primaria)
     Apellidos VARCHAR(255),                                            -- Apellidos del conductor
     Nombre VARCHAR(255),                                               -- Nombre del conductor
