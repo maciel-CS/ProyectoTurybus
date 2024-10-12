e4b4b0a49df8f1950ec194f88f4d69d8bf87abc1
gabriela-re <73082628+gabriela-re@users.noreply.github.com>
2024-10-11 23:08:51 -0400

    Update GetRutaTuristicaChangesByRowVersion.sql

--- SolutionTurybus/TuryBuzOLTP/Schema/Programmability/Stored Procedures/GetRutaTuristicaChangesByRowVersion.sql
+++ SolutionTurybus/TuryBuzOLTP/Schema/Programmability/Stored Procedures/GetRutaTuristicaChangesByRowVersion.sql
@@ -8,7 +8,9 @@ BEGIN
 	SELECT  id, 
             id_lugarOrigen,	
             id_LugarDestino,                                                    
-	          Costo_Transporte
+		Costo_Transporte
+		tiempoParada,
+		tiempo_viaje
 	  FROM [dbo].[RutaTuristica]
 	  WHERE [rowversion] > CONVERT(ROWVERSION,@startRow) 
 	  AND [rowversion] <= CONVERT(ROWVERSION,@endRow)
